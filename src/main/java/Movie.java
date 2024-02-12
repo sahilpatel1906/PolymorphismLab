0ef56e44fe2ec5ff925355c8050b37aeab330ee3
Sahil Patel <sahilpatel1906@gmail.com>
2024-02-12 10:58:15 -0500

    Created Movie Class

---
+++ src/main/java/Movie.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Movie {
+    protected String name;
+    protected ArrayList<String> characters;
+
+    public Movie (String name) {
+        this.name = name;
+    }
+}
