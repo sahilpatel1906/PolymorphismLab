947440ac8d96acb6b2bd89f85224ed5225071d49
Sahil Patel <sahilpatel1906@gmail.com>
2024-02-12 11:31:46 -0500

    Created GobletOfFire Class

---
+++ src/main/java/GobletOfFire.java
@@ -0,0 +1,11 @@
+import java.sql.Time;
+
+public class GobletOfFire extends HarryPotterMovie {
+    private Time duration;
+    private int rating;
+
+    public GobletOfFire(String name) {
+        super(name);
+    }
+
+}
