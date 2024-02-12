5ab2561144b4723aed08067733da19f89b78a263
Sahil Patel <sahilpatel1906@gmail.com>
2024-02-12 10:53:12 -0500

    Created a Theatre Class

---
+++ src/main/java/Theatre.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Theatre {
+    private String theatreName;
+    private ArrayList<Movie> movies ;
+
+    public Theatre(String theatreName) {
+        this.theatreName = theatreName;
+    }
+}
