7f5fc299183f60da0b9f0756479923a0be208865
Sahil Patel <sahilpatel1906@gmail.com>
2024-02-12 11:40:23 -0500

    Created PhilosophersStone Class

---
+++ src/main/java/PhilosophersStone.java
@@ -0,0 +1,14 @@
+import java.sql.Time;
+
+public class PhilosophersStone extends HarryPotterMovie{
+    private Time duration;
+    private int rating;
+
+    public PhilosophersStone(String name){
+        super(name);
+    }
+    
+    public String printSummary() {
+        return "first movie.";
+    }
+}
